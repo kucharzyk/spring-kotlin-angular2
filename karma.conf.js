359e6f14795bb3ab866e384fe09028078023fac7
Tomasz Kucharzyk <kucharzyk.tomasz@gmail.com>
2016-11-17 19:45:42 +0100

    updated angular and angular-cli

--- karma.conf.js
+++ karma.conf.js
@@ -13,11 +13,15 @@ module.exports = function (config) {
       require('angular-cli/plugins/karma')
     ],
     files: [
-      { pattern: './src/main/typescript/test.ts', watched: false }
+      {pattern: './src/main/typescript/test.ts', watched: false}
     ],
     preprocessors: {
       './src/main/typescript/test.ts': ['angular-cli']
     },
+    mime: {
+      'text/x-typescript': ['ts', 'tsx']
+    },
+
     remapIstanbulReporter: {
       reports: {
         html: 'coverage',
@@ -28,12 +32,15 @@ module.exports = function (config) {
       config: './angular-cli.json',
       environment: 'dev'
     },
-    reporters:  ['progress', 'karma-remap-istanbul'],
+    reporters: config.angularCli && config.angularCli.codeCoverage
+      ? ['progress', 'karma-remap-istanbul']
+      : ['progress'],
+
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],// could use 'PhantomJS'
+    browsers: ['Chrome'],
     singleRun: false,
 
     browserNoActivityTimeout: 100000
