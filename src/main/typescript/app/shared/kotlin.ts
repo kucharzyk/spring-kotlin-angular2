214c5b05ac4e85438e38c2c3c37c24118984e534
Tomasz Kucharzyk <kucharzyk.tomasz@gmail.com>
2016-11-18 21:04:02 +0100

    typescript generator configuration

---
+++ src/main/typescript/app/shared/kotlin.ts
@@ -0,0 +1,15 @@
+
+namespace Kotlin {
+
+    export interface UsernamePasswordDto {
+        username: string;
+        password: string;
+    }
+
+    export interface SimpleDto {
+        text: string;
+        version: string;
+        date: Date;
+    }
+
+}
